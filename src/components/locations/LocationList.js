44a8f00e6a351d155f65fc67390236fb977f334f
Dylan Morris <dylanrobertmorris@gmail.com>
2021-03-05 10:15:50 -0600

    compiled location provider

---
+++ src/components/locations/LocationList.js
@@ -0,0 +1,5 @@
+import React, { useContext, useEffect } from "react"
+import { LocationContext } from "./LocationProvider"
+import { LocationCard } from "./Location"
+import "./Location.css"
+import { useHistory } from "react-router-dom"
---
+++ src/components/locations/LocationProvider.js
@@ -0,0 +1,50 @@
+import React, { useState, createContext } from "react"
+
+export const LocationContext = createContext()
+
+export const LocationProvider = (props) => {
+
+    const [locations, setLocations] = useState([])
+
+    const getLocations = () => {
+        return fetch("http://localhost:8088/locations")
+            .then(res => res.json())
+            .then(setLocations)
+    }
+
+    const addLocation = (locationObj) => {
+        return fetch("http://localhost:8088/locations", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(locationObj)
+        })
+        .then(getLocations)
+    }
+
+    const getLocationById = (id) => {
+        return fetch(`http://localhost:8088/locations/${id}`)
+            .then(res => res.json())
+    }
+
+    const updateLocation = location => {
+        return fetch(`http://localhost:8088/locations/${location.id}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(location)
+        })
+        .then(getLocations)
+    }
+
+    return (
+        <LocationContext.Provider value ={{
+            locations, getLocations, addLocation, getLocationById, updateLocation
+        }}>
+            {props.children}
+        </LocationContext.Provider>
+    )
+
+}
