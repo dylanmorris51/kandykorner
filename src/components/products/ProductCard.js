ab491146e84d5bc6098e36a0fc177413c4cad45d
Dylan Morris <dylanrobertmorris@gmail.com>
2021-03-05 13:32:33 -0600

    touch ups

--- src/components/products/ProductCard.js
+++ src/components/products/ProductCard.js
@@ -6,6 +6,6 @@ export const ProductCard = ({product}) => (
     <section className="product">
         <h3 className="product__name">{product.name}</h3>
         <div className="product__price">{product.price}</div>
-        <div className="product__type">Product Type Foreign Key: {product.productTypeId}</div>
+        <div className="product__type">{product.productType.name}</div>
     </section>
 )
