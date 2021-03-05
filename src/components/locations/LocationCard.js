03169a7b2f2640c8e8cffe49712127fdcd4a73c1
Dylan Morris <dylanrobertmorris@gmail.com>
2021-03-05 10:20:13 -0600

    added LocationCard.js

---
+++ src/components/locations/LocationCard.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { Link } from "react-router-dom"
+import "./Location.css"
+
+export const LocationCard = ({location}) => (
+        <section className="location">
+            <h3 className="location__name">
+                <Link to={`/locations/detail/${location.id}`}>
+                    {location.name}
+                </Link>
+            </h3>
+        </section>
+
+)
--- src/components/locations/LocationList.js
+++ src/components/locations/LocationList.js
@@ -3,3 +3,28 @@ import { LocationContext } from "./LocationProvider"
 import { LocationCard } from "./Location"
 import "./Location.css"
 import { useHistory } from "react-router-dom"
+
+export const LocationList = () => {
+
+    const { locations, getLocations } = useContext(LocationContext)
+
+    useEffect(() => {
+        getLocations()
+    }, [])
+
+    const history = useHistory()
+
+    return (
+        <>
+            <h2>Locations:</h2>
+                <div className="locations__list">
+                    {
+                        locations.map(location => {
+                            return <LocationCard key={location.id} location={location} />
+                        })
+                    }
+                </div>
+        </>
+    )
+
+}
